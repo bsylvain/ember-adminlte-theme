fccd66a0f9eec7e8fdd7ac200699da3ac1449985
Sylvain <sylvain@sylvain-ubuntu.net>
2018-08-29 22:40:37 +0200

    update

--- addon/components/content-wrapper.js
+++ addon/components/content-wrapper.js
@@ -7,21 +7,21 @@ export default Ember.Component.extend({
 
     self.resizeElement();
     $(window).resize(function(){
-      self.resizeElement()
-    })
+      self.resizeElement();
+    });
   },
   resizeElement: function(){
     var headHeight = $('.main-header').outerHeight();
     var footerHeight = $('.main-footer').outerHeight();
     var sidebar_height = $(".sidebar").height();
     
-    var neg = headHeight + footerHeight
+    var neg = headHeight + footerHeight;
     var window_height = $(window).height();
     
     if ($("body").hasClass("fixed")) {
-      this.setHeight(window_height - footerHeight)
+      this.setHeight(window_height - footerHeight);
     } else {
-      this.setHeight(window_height >= sidebar_height ? (window_height - neg) : sidebar_height)
+      this.setHeight(window_height >= sidebar_height ? (window_height - neg) : sidebar_height);
     }
   },
   setHeight: function(height){
